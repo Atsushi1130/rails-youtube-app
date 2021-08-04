84bc03d180d3f5d52f1c056b56f91786c77ba659
Atsushi1130 <atsu.1130.2b@gmail.com>
2021-08-04 18:40:27 +0900

    0804

--- app/controllers/youtube_controller.rb
+++ app/controllers/youtube_controller.rb
@@ -3,7 +3,7 @@ class YoutubeController < ApplicationController
 
   def find_videos(keyword)
     service = Google::Apis::YoutubeV3::YouTubeService.new
-    service.key = 'API-KEY'
+    service.key = 'AIzaSyCfYzxEOz5wNahVUMS11Hjdhf2-9VajLhQ'
 
     next_page_token = nil
     opt = {
